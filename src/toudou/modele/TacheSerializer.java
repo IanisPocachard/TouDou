6d9c4fdb6cac379b67a4e4d3e56db7d6f6a05138
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2025-12-10 17:49:13 +0100

    Classe Serializer finie

--- src/toudou/modele/TacheSerializer.java
+++ src/toudou/modele/TacheSerializer.java
@@ -1,5 +1,8 @@
 package modele;
 
+import java.io.*;
+import java.util.List;
+
 public class TacheSerializer {
 
     private static final String chemin = "src/toudou/taches.toudou";
@@ -19,4 +22,27 @@ public class TacheSerializer {
         }
         return instance;
     }
+
+    // ################
+    // # PARTIE OBJET #
+    // ################
+
+    public static void ecrireFichier(List<Tache> taches) throws Exception {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(chemin))) {
+            oos.writeObject(taches);
+        }
+    }
+
+    public static List<Tache> lireFichier() throws Exception {
+        File f = new File(chemin);
+
+        if (!f.exists()) {
+            return null;
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(chemin))) {
+            return (List<Tache>) ois.readObject();
+        }
+    }
+
 }
