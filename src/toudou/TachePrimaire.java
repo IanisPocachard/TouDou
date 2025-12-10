697bcee117293a0ce741e611ccc83988e917c6f6
Néo BOULANGER <neo@MacBook-Air-de-Neo.local>
2025-12-10 14:48:19 +0100

    finition de la déclaration des taches

--- src/toudou/TachePrimaire.java
+++ src/toudou/TachePrimaire.java
@@ -1,2 +1,38 @@
-public class TachePrimaire {
+import java.util.Date;
+
+public class TachePrimaire extends Tache{
+    private Date dateDebut;
+    private Date dateEcheance;
+    private boolean archivee;
+
+    public TachePrimaire(String nom, String description, int numTache, Date dateDebut, Date dateEcheance) {
+        super(nom, description, numTache);
+        this.dateDebut = dateDebut;
+        this.dateEcheance = dateEcheance;
+        this.archivee = false;
+    }
+
+    /**
+     * recupere la date de debut de la tâche
+     * @return dateDebut
+     */
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    /**
+     * recupere la date de fin de la tâche
+     * @return dateEcheance
+     */
+    public Date getDateEcheance() {
+        return dateEcheance;
+    }
+
+    /**
+     * retourne vrai si la tache est archivée
+     * @return archivee
+     */
+    public boolean isArchivee() {
+        return archivee;
+    }
 }
