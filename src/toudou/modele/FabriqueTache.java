77761b1f485eb584cc48c7264b5bbefad3a51269
CinarErdal <cinarerdal5838@gmail.com>
2025-12-12 09:01:59 +0100

    ajout methode creertache

--- src/toudou/modele/FabriqueTache.java
+++ src/toudou/modele/FabriqueTache.java
@@ -1,4 +1,10 @@
 package modele;
 
+import java.util.Date;
+
 public class FabriqueTache {
+
+    public Tache creerTache(String nom, String description, Date dateEcheance, int tempsNecessaire) throws Exception {
+        return new Tache(nom, description, dateEcheance, tempsNecessaire);
+    }
 }
