e64a0ab0a6a96419774b2372d7850ba635996a3d
Maxime Craincourt <maxime.craincourt7@etu.univ-lorraine.fr>
2025-12-12 08:48:51 +0100

    Modif testtacheprimaire

--- src/test/TestTachesPrimaire.java
+++ src/test/TestTachesPrimaire.java
@@ -8,7 +8,7 @@ public class TestTachesPrimaire {
 
     @Test
     public void test_01_Construction_normale() throws Exception {
-        Tache t = new Tache("Test", "Test des tâches pour vérifier la construction normale", 1);
+        Tache t = new Tache("Test", "Test des tâches pour vérifier la construction normale", 1, 10);
         assertNotNull(t, "L'objet Tache ne doit pas être null");
         assertEquals("Le nom n'est pas correctement initialisé", t.getNom(), "Test");
         assertEquals("La description n'est pas correctement initialisée", t.getDescription(), "Test des tâches pour vérifier la construction normale");
