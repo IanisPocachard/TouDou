54a327331e30842ea503843d2f372969d842545e
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2025-12-15 17:45:06 +0100

    Ajout Modele (structure)

---
+++ src/toudou/modele/Modele.java
@@ -0,0 +1,35 @@
+package modele;
+
+import vue.Observateur;
+
+import java.util.ArrayList;
+
+public class Modele extends Sujet {
+
+    private ArrayList<Observateur> observateurs;
+
+    private ArrayList<Tache> taches;
+
+    public Modele() {
+        this.observateurs = new ArrayList<>();
+        this.taches = new ArrayList<>();
+    }
+
+    @Override
+    public void ajouterObservateur(Observateur o) {
+        this.observateurs.add(o);
+    }
+
+    @Override
+    public void retirerObservateur(Observateur o) {
+        this.observateurs.remove(o);
+    }
+
+    @Override
+    public void notifierObservateurs() {
+        for (Observateur o : observateurs) {
+            o.actualiser(this);
+        }
+    }
+
+}
