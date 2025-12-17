56d80a7f8493bd63e0d30ddb6f29fc53916ea92e
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2025-12-17 09:04:43 +0100

    méthode ajouterLog

--- src/toudou/vue/VueHistorique.java
+++ src/toudou/vue/VueHistorique.java
@@ -7,16 +7,27 @@ import modele.Sujet;
 public class VueHistorique extends TextArea implements Observateur {
 
     private Modele modele;
+    private String texte;
 
-    public VueHistorique(Sujet s) {
-        if (s instanceof Modele) {
-            this.modele = (Modele) s;
-        }
+    public VueHistorique(Modele m) {
+        this.modele = m;
+        this.texte = "Bienvenue sur TouDou !";
+        this.setText(texte);
+
+        this.setEditable(false);
+        this.setWrapText(true);
+
+        modele.ajouterObservateur(this);
     }
 
     @Override
     public void actualiser(Sujet s) {
+        this.setText(texte);
+    }
 
+    public void ajouterLog(String txt) {
+        this.texte += txt + "\n";
+        this.actualiser(modele);
     }
 
 }
