d87f1bb8c93e44ec2d97a7e6353749e334fe2cc3
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2025-12-19 08:36:57 +0100

    Modif disposition Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -37,6 +37,26 @@ public class Main extends Application {
         Button btnDiagGantt = new Button("Diagramme de Gantt");
         btnDiagGantt.setOnAction(e -> VueFormGantt.afficher(modele));
         Button btnTachesArchive = new Button("Tâches archivées");
+
+        // Style des boutons
+        String styleBouton = """
+            -fx-background-color: #3b82f6;
+            -fx-text-fill: white;
+            -fx-font-size: 14px;
+            -fx-font-weight: bold;
+            -fx-padding: 8 16 8 16;
+            -fx-background-radius: 8;
+        """;
+
+        btnListeTaches.setStyle(styleBouton);
+        btnKanban.setStyle(styleBouton);
+        btnDiagGantt.setStyle(styleBouton);
+        btnTachesArchive.setStyle(styleBouton);
+
+        boutons.setSpacing(10);
+        boutons.setStyle("-fx-padding: 10;");
+
+
         boutons.getChildren().addAll(btnListeTaches, btnKanban, btnDiagGantt, btnTachesArchive);
 
 
@@ -75,7 +95,15 @@ public class Main extends Application {
 
 
         // AJOUT DE TOUS LES ELEMENTS
-        screen.getChildren().addAll(boutons, liste, kanban, historique);
+        HBox elements = new HBox(20);
+        VBox partieGauche = new VBox();
+        VBox partieDroite = new VBox();
+
+        partieGauche.getChildren().addAll(liste);
+        partieDroite.getChildren().addAll(kanban, historique);
+        elements.getChildren().addAll(partieGauche, partieDroite);
+
+        screen.getChildren().addAll(boutons, elements);
 
         Scene scene = new Scene(screen);
         primaryStage.setScene(scene);
