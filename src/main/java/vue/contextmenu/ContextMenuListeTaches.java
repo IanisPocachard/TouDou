8c1cf9f166b15ab91b90c933c743973fcef5ce09
IanisPocachard <ianis.pocachard9@etu.univ-lorraine.fr>
2026-01-08 08:17:19 +0100

    Ajout classe ContextMenuListe

---
+++ src/main/java/vue/contextmenu/ContextMenuListeTaches.java
@@ -0,0 +1,25 @@
+package vue.contextmenu;
+
+import controller.ControlFormAddSousTache;
+import controller.ControlFormAddTache;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import modele.Modele;
+import vue.VueListe;
+
+public class ContextMenuListeTaches extends ContextMenu {
+
+    public ContextMenuListeTaches(Modele modele, VueListe vueListe) {
+        MenuItem itemAjouterTache = new MenuItem("Ajouter une tâche");
+        MenuItem itemAjouterSousTache = new MenuItem("Ajouter une sous-tâche");
+        MenuItem itemAjouterDependance = new MenuItem("Ajouter une dépendance");
+        MenuItem itemArchiver = new MenuItem("Archiver la tâche");
+        MenuItem itemSupprimerTache = new MenuItem("Supprimer la tâche");
+
+        itemAjouterTache.setOnAction(new ControlFormAddTache(modele));
+        itemAjouterSousTache.setOnAction(new ControlFormAddSousTache(modele, vueListe.getTacheSelectionnee()));
+
+        this.getItems().addAll(itemAjouterTache, itemAjouterSousTache, itemAjouterDependance, itemArchiver, itemSupprimerTache);
+    }
+
+}
